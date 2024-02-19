a626f554ecfc6ca63741dea819de34badce5ce01
Sneha Sapkota <108715148+sneha164@users.noreply.github.com>
2024-02-19 18:43:27 +0545

    Add files via upload

---
+++ Last40/assets/js/main.js
@@ -0,0 +1,16 @@
+function slide1() {
+    var splashScreen = document.getElementById('splashScreen');
+    var content = document.getElementById('content');
+
+    splashScreen.style.display = 'none';
+    content.style.display = 'block';
+}
+
+function slide2() {
+    var splashScreen = document.getElementById('splashScreen');
+    var content = document.getElementById('content');
+
+    splashScreen.style.display = 'block';
+    content.style.display = 'none';
+}
+
